132120e149ea86bc1ea44dbe9d8e5bbf33349e29
Paweł Szczepankiewicz <pawel.szczepankiewicz@outlook.com>
2021-08-20 08:09:38 +0200

    First commit

---
+++ app.js
@@ -0,0 +1,11 @@
+const app = Vue.createApp({
+    data() {
+        return {
+            title: 'Empire',
+            author: 'Meme',
+            age: 10
+        }
+    }
+})
+
+app.mount('#app')
