c2b385d69448482dd93449cf4cd504da7870e5ca
pawel.szczepankiewicz <pawel.szczepankiewicz@enginiety.com>
2021-08-22 15:58:42 +0200

    Created first projects

---
+++ vue-modal-project/src/App.vue
@@ -0,0 +1,60 @@
+<template>
+  <h1>{{ title }}</h1>
+  <div v-if="showModal">
+    <Modal theme="sale" @close="toggleModal">
+      <template v-slot:links>
+        <a href="#">Sign up now</a>
+        <a href="#">Sign up later</a>
+      </template>
+      <h1>Sign up!</h1>
+      <p>For more info</p>
+    </Modal>
+  </div>
+
+  <div v-if="showModalTwo">
+    <Modal @close="toggleModalTwo">
+      <h1>Sign up!</h1>
+      <p>For more info</p>
+    </Modal>
+  </div>
+  <button @click="toggleModal">Open Modal</button>
+  <button @click="toggleModalTwo">Open Modal Two</button>
+</template>
+
+<script>
+import Modal from "./components/Modal.vue";
+
+export default {
+  name: "App",
+  components: { Modal },
+  data() {
+    return {
+      title: "My First Vue App",
+      showModal: false,
+      showModalTwo: false,
+    };
+  },
+  methods: {
+    toggleModal() {
+      this.showModal = !this.showModal;
+    },
+    toggleModalTwo() {
+      this.showModalTwo = !this.showModalTwo;
+    },
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+h1 {
+  color: coral;
+}
+</style>
---
+++ vue-reaction-timer/src/components/Results.vue
@@ -0,0 +1,31 @@
+<template>
+  <p>Reaction time: {{ score }} ms</p>
+  <p class="rank">{{ rank }}</p>
+</template>
+
+<script>
+export default {
+  props: ["score"],
+  data() {
+    return {
+      rank: null,
+    };
+  },
+  mounted() {
+    if (this.score < 250) {
+      this.rank = "Ninja";
+    } else if (this.score < 400) {
+      this.rank = "Rapid";
+    } else {
+      this.rank = "Snail";
+    }
+  },
+};
+</script>
+<style>
+.rank {
+  font-size: 1.4em;
+  color: green;
+  font-weight: bold;
+}
+</style>
