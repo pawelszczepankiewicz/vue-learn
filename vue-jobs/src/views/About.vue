aee17dddd11d7cca437b50f960d51d03ce23bc97
Paweł Szczepankiewicz <pawel.szczepankiewicz@outlook.com>
2021-08-24 18:29:17 +0200

    Added routes logic

--- vue-jobs/src/views/About.vue
+++ vue-jobs/src/views/About.vue
@@ -1,5 +1,13 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <p>
+      Lorem ipsum dolor sit, amet consectetur adipisicing elit. Id beatae rem
+      quaerat sunt tempora. Nemo ipsa dolorum adipisci natus! Rerum quae
+      reiciendis aperiam accusamus! Reprehenderit, explicabo, aperiam harum
+      tempore temporibus minus odio incidunt facere dolorum ipsam, molestiae
+      cumque omnis facilis ullam! Ab magnam incidunt atque eius perferendis
+      deserunt harum rerum?
+    </p>
   </div>
 </template>
