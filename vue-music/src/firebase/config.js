49f97bb0d886c0c4fb4ccea501a7c7993fc57f34
pawel.szczepankiewicz <pawel.szczepankiewicz@enginiety.com>
2021-08-30 13:19:11 +0200

    New playlists project

---
+++ vue-music/src/firebase/config.js
@@ -0,0 +1,22 @@
+import firebase from 'firebase/app'
+import 'firebase/firestore'
+import 'firebase/auth'
+import 'firebase/storage'
+
+const firebaseConfig = {
+
+};
+
+// init firebase
+firebase.initializeApp(firebaseConfig)
+
+// init services
+const projectFirestore = firebase.firestore()
+const projectAuth = firebase.auth()
+const projectStorage = firebase.storage()
+
+// timestamp
+const timestamp = firebase.firestore.FieldValue.serverTimestamp
+
+export { projectFirestore, projectAuth, projectStorage, timestamp }
+
